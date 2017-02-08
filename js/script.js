c7a033016393a0a96c01c5f4375a9f2e767295cf
whogotnibs <thomas.bell@rogers.com>
2017-02-08 03:06:15 -0500

    pixel art

--- js/script.js
+++ js/script.js
@@ -13,8 +13,9 @@ var rectangles = [];
 const TOTAL_HEALTH = 5;
 var health = 3;
 //interval of health degeneration (milliseconds)
-var interval = 1000*60;
+var interval = 1000*10;
 var seenInstructions = false;
+var seenDeath = false;
 
 $(document).ready(function() {
 
@@ -24,17 +25,25 @@ $(document).ready(function() {
 
   instructions();
 
-// start function on click
+  // death();
+
+  // start function on click
   $('.continue').click(clickedContinue);
 
   // when the water button is clicked the plants health will go up by +1
   $('.water').click(clickedWater);
 
   // every interval the plant's health will go down by -1
-  degeneration();
   setInterval(degeneration, interval);
 
+  // making the health bar
   createRectangles();
+
+  // filling the rest of the health bar
+  createRemainingRectangles();
+
+  // animating the flower based on its health
+  pixelArt();
 
 });
 
@@ -57,22 +66,58 @@ function setUpCheats() {
       console.log("Cheat!");
     }
   });
+  $(document).keypress(function (event) {
+    if (event.which == 48) {
+      // 0
+      health = 0;
+      localStorage.setItem('health',0);
+      console.log("ded...");
+    }
+  });
+  $(document).keypress(function (event) {
+    if (event.which == 49) {
+      // 1
+      health = 1;
+      localStorage.setItem('health',1);
+      console.log("1 life...");
+    }
+  });
+  $(document).keypress(function (event) {
+    if (event.which == 50) {
+      // 2
+      localStorage.clear();
+      console.log("localStorage cleared...");
+    }
+  });
 }
 
+//this makes it so that if the player has let the plant die they can only ever
+//see the death screen. u cant bring *this* flower back to life
+function death() {
+  seenDeath = localStorage.getItem('seenDeath');
+  if (seenDeath = true) {
+    console.log("Showing death...");
+    seenDeath = true;
+    // show the death screen
+    $('.start, .continue').hide() ;
+    $('.plant, .health, .water').hide() ;
+    $('.death').show();
+  }
+}
+
+//this function lets the player skip the instructions if they have already started
+//the life of their flower
 function instructions() {
   seenInstructions = localStorage.getItem('seenInstructions');
-  if (seenInstructions == undefined) {
-    console.log("Showing instructions...");
-    seenInstructions = true;
-    // show the instructions
-    $('.start, .continue').show() ;
-    ;
-  }
-  else {
-    seenInstructions = true;
+  if (seenInstructions = true) {
     // show the game's main divs
     $('.plant, .health, .water').show() ;
     $('.start, .continue').hide() ;
+  }
+  else {
+    console.log("Showing instructions...");
+    // show the instructions
+    $('.start, .continue').show() ;
   }
 }
 
@@ -86,6 +131,10 @@ function clickedContinue() {
   $('.plant, .health, .water').css({ 
     display: 'block'
   });
+
+  //record that the user has already read the instructions
+  seenInstructions = true;
+  localStorage.setItem('seenInstructions', seenInstructions);
 }
 
 function clickedWater() {
@@ -103,6 +152,8 @@ function degeneration() {
 
   //when the plant health reaches 0 the death message will be displayed
   if (health <= 0) {
+    health = 0;
+    seenDeath = true;
     $('.start, .continue').css({ 
       display: 'none'
     });
@@ -114,14 +165,16 @@ function degeneration() {
     });
   }
   localStorage.setItem('health', health);
+  localStorage.setItem('seenDeath', seenDeath);
 }
 
-function createRectangles () {
+function createRectangles() {
   // create an array to store our rectangles
   rectangles = new Array(TOTAL_HEALTH);
 
   // loop through the (empty) array, putting rectangles in it
-  for (var i = 0; i < TOTAL_HEALTH; i++) {
+  // based on the plants health
+  for (var i = 0; i < health; i++) {
 
     // Make a rectangle
     var rect = rectangle(15, 15);
@@ -153,11 +206,59 @@ function rectangle (w, h) {
     width: rect.w + 'px',
     height: rect.h + 'px', 
     margin: '15px',
-    backgroundColor: 'lightblue',
+    backgroundColor: 'skyblue',
   }); 
 
 
   // Finally, we RETURN the div we created from the function
   // So that whoever called this function can do something with it
   return rect;
+}
+
+function createRemainingRectangles() {
+  // create an array to store the remaining rectangles in the health bar
+  remainingRectangles = new Array(TOTAL_HEALTH);
+
+  // loop through the (empty) array, putting rectangles in it
+  // based on the plants lost health
+  for (var i = 0; i < (TOTAL_HEALTH - health); i++) {
+
+    // Make a rectangle
+    var emptyRect = emptyRectangle(15, 15);
+
+    // put it in the array and add it to the body
+    remainingRectangles[i] = emptyRect;
+    $('.health').append(remainingRectangles[i]);
+  }
+}
+
+function emptyRectangle (w, h) {
+  // First we create the div that will be the rectangle using jQuery
+  var emptyRect = $('<div></div>'); 
+
+  // We can store properties in our rectangle object by just adding
+  // them explicitly.
+  emptyRect.w = w;
+  emptyRect.h = h;
+
+  // Then we set up the CSS of the div so that it looks like a rectangle
+  // in the location we want it.
+  emptyRect.css({ 
+    display: 'inline-block',
+    width: emptyRect.w + 'px',
+    height: emptyRect.h + 'px', 
+    margin: '15px',
+    backgroundColor: 'lightgrey',
+  }); 
+
+  // Finally, we RETURN the div we created from the function
+  // So that whoever called this function can do something with it
+  return emptyRect;
+}
+
+function pixelArt() {
+  var imageSource = "images/plant" + health + ".png";
+  var image = $('<img src="' + imageSource + '">');
+  $('.plant').append(image);
+
 }
